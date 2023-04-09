d3dae8cfc6a8d6099322a81211176d9ab05e573f
Vitaliy <fruitamin@yandex.ru>
2023-04-10 01:11:50 +0300

    Классы ES6

--- js/oop_5.js
+++ js/oop_5.js
@@ -44,3 +44,67 @@ console.log(jack instanceof Person) // true
 // Устанавливаем конструктор
 Student.prototype.constructor = Student
 console.dir(Student.prototype.constructor)
+
+/*
+    Классы ES6
+*/
+
+class Person1 {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName // При создании будет запущен setter
+        this.birthYear = birthYear
+    }
+
+    // Методы добавляются в свойство .prototype
+    printAge() {
+        console.log(new Date().getFullYear() - this.birthYear)
+    }
+
+    greet() {
+        console.log(`Hello! My name is ${this.fullName}`)
+    }
+
+    get age() {
+        return new Date().getFullYear() - this.birthYear
+    }
+
+    // Установить значение в уже существующее свойство
+    set fullName(fullName) {
+        if (fullName.includes(' ')) {
+            this._fullName = fullName // если сеттер вызывается из конструктора добавляем _
+            return
+        }
+
+        alert('Необходимо ввести полное имя!')
+    }
+
+    get fullName() {
+        return this._fullName
+    }
+
+    static highFive() {
+        console.log('High five!')
+    }
+}
+
+class Student1 extends Person1 {
+    constructor(fullName, birthDate, dept) {
+        // Обязательно записать в самом начале
+        super(fullName, birthDate);
+        this.dept = dept
+    }
+
+    introduce() {
+        console.log(`My name is ${this.fullName} and I study at the ${this.dept} department.`)
+    }
+
+    greet() {
+        console.log(`Hello! My name is ${this.fullName}, i'm a student`)
+    }
+}
+
+const test = new Student1('test test', 2005, 'Programming')
+console.log(test)
+test.introduce()
+test.greet()
+
