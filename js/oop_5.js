93ddbe59a5ebbdaa33237d10cdb88989f5ef2e23
Vitaliy <fruitamin@yandex.ru>
2023-04-10 01:38:56 +0300

    Наследование Object.create()

--- js/oop_5.js
+++ js/oop_5.js
@@ -46,7 +46,7 @@ Student.prototype.constructor = Student
 console.dir(Student.prototype.constructor)
 
 /*
-    Классы ES6
+    2. Классы ES6
 */
 
 class Person1 {
@@ -108,3 +108,31 @@ console.log(test)
 test.introduce()
 test.greet()
 
+/*
+    3. Object.create()
+*/
+const PersonProto = {
+    printAge() {
+        console.log(new Date().getFullYear() - this.birthYear)
+    },
+
+    initPerson(firstName, birthYear) {
+        this.firstName = firstName
+        this.birthYear = birthYear
+    }
+}
+
+const StudentProto = Object.create(PersonProto)
+
+StudentProto.initStudent = function (firstName, birthYear, dept) {
+    PersonProto.initPerson.call(this, firstName, birthYear)
+    this.dept = dept
+}
+
+StudentProto.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study at the ${this.dept} department.`)
+}
+
+const jack1 = Object.create(StudentProto)
+jack1.initStudent('Jack', 2003, 'Programming')
+jack1.introduce()
